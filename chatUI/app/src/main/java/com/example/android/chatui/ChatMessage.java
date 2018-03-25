d7eea6c24387ac0736458fe01ae7b7cb7de341e5
rohangho <rohangho@live.com>
2018-03-26 02:24:30 +0530

    Added the chat Ui

---
+++ chatUI/app/src/main/java/com/example/android/chatui/ChatMessage.java
@@ -0,0 +1,25 @@
+package com.example.android.chatui;
+
+/**
+ * Created by ROHAN on 26-03-2018.
+ */
+
+public class ChatMessage {
+
+    private String content;
+    private boolean isMine;
+
+    public ChatMessage(String content, boolean isMine) {
+        this.content = content;
+        this.isMine = isMine;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public boolean isMine() {
+        return isMine;
+    }
+
+}
