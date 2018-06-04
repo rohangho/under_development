c5151d0e5824118ee88f4e039593b3ea6b6d92f3
rohangho <rohangho@live.com>
2018-06-04 18:50:27 +0530

    added the swipe features

---
+++ SwipeUp/app/src/main/java/com/example/android/swipeup/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.android.swipeup;
+
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+
+        // Add Fragments to adapter one by one
+        adapter.addFragment(new Fragment_one(), "FRAG1");
+        adapter.addFragment(new Fragment_two(), "FRAG2");
+        adapter.addFragment(new Fragment_three(), "FRAG3");
+        viewPager.setAdapter(adapter);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+
+    }
+}
